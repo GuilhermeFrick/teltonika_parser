bd2cd6cf6e583dd0f2c050852952e21917603715
GuilhermeFrick <frickoliveira.ee@gmail.com>
2025-07-19 01:40:50 -0300

    fix: corrigindo versao do jest

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,13 @@ const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
+/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.ts$': ['ts-jest']
   },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };
